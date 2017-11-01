e4f54de3e4d71a9dbaee446d0acc79550a2ffabe
Margaretha Juan <tulip_holy@yahoo.com>
2017-11-01 02:35:42 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,12 +1,5 @@
 # code your #position_taken? method here!
 
 def position_taken(board, index)
-  i = 0
-  while i < board.size && !isTaken do
-    if(board[i] != nil && (board[i] == "X" || board[i] == "O"))
-      return true
-    end
-    i = i + 1
-  end
   return false
 end
