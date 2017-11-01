b6911ba4d725624ef65d163859122fbff579ae8d
Margaretha Juan <tulip_holy@yahoo.com>
2017-11-01 02:17:35 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,13 @@
 
 def position_taken(board, index)
   isTaken = false
-
+  i = 0
+  while i < board.size && !isTaken do
+    if(board[i] != nil || board[i] == "X" || board[i] == "O")
+      isTaken = true
+    end
+    i = i + 1
+  end
   for element in board do
     if element == nil || element == "X" || element == "O"
       isTaken = false
